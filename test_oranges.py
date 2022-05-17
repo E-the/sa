9c94ad96b3b94b25042f326a5c3c46bb7cd2c341
E-the <ashmitashrestha564@gmail.com>
2022-05-17 12:44:10 +0545

    saaA

--- test_oranges.py
+++ test_oranges.py
@@ -1,49 +1,30 @@
 """Orange Basket feature tests."""
 
-
-
-# pytest-bdd generate features/oranges.feature > tests/acceptance/test_oranges_steps.py
-
 from pytest_bdd import (
-
     given,
-
     scenarios,
-
     then,
-
     when,
-
     parsers
-
 )
 
 
+from project.oranges import OrangeBasket # To make a basket.
 
-from project.oranges  import OrangeBasket
+scenarios("../../features/oranges.feature")
+def test_oranges_to_a_basket():
 
-
-
-scenarios('../../feature/oranges.feature')
-def test_add_oranges_to_a_basket():
-
-    """Add oranges to a basket."""
+    """Add or remove oranges to a basket."""
 
 
 
+@given(parsers.parse('the basket has {initial:d} oranges'), target_fixture='basket') 
+        # target_fixture gives variable name for the returned value of a function.
 
-@given(parsers.parse('the basket has {initial:d} oranges'),target_fixture='basket')
-
-def the_basket_has_2_oranges(initial):
-
+def the_basket_has_2_oranges(initial): 
     """the basket has 2 oranges."""
-
-    # raise NotImplementedError
-
-    basket=OrangeBasket(initial_count=initial)
-
-    return  basket
-
+    basket = OrangeBasket(initial_count= initial) # Make a basket with 2 oranges.
+    return basket
 
 
 @when(parsers.parse('{more:d} oranges are added to the basket'))
@@ -51,24 +32,17 @@ def the_basket_has_2_oranges(initial):
 def oranges_are_added_to_the_basket(basket, more):  # basket is the same as the basket in line 19
     """4 oranges are added to the basket."""
     basket.add(more)
-    
-@when(parsers.parse('{some:d} oranges are removed'))
 
-def oranges_are_removed(basket,some):
-
-    """4 oranges are added to the basket."""
-
-    # raise NotImplementedError
-
-    basket.remove(some)    
-
-
+@when(parsers.parse('{some:d} oranges are removed from the basket'))
+def oranges_are_removed(basket, some): {
+    basket.remove(some)
+}
 
 
 @then(parsers.parse('the basket contains {final:d} oranges'))
+def the_basket_contains_6_oranges(basket, final):   # basket is the same as the basket in line 19
+    """Check if the basket contains 6 oranges."""
+    assert basket.count == final
 
-def the_basket_contains_6_oranges(basket,final):
 
-    """the basket contains 6 oranges."""
-
-    assert basket.count==final
+# python -m test to run pytest
